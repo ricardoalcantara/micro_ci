684b4e3f434d90ee362389c67459a819f4a991d3
Ricardo da Silva Alcantara <ricardo.salcantara@dataprev.gov.br>
2020-09-22 17:50:19 -0300

    cloning and building...

--- src/controllers/WebHook.ts
+++ src/controllers/WebHook.ts
@@ -1,40 +1,84 @@
 import express, { Request, Response, Router } from "express";
-
-
 import { Clone, Cred, CloneOptions } from "nodegit";
 import Docker from "dockerode";
 import tar, { CreateOptions, FileOptions } from "tar";
+import path from "path";
+import fs_old from "fs";
+import rimraf from "rimraf";
+
+const fs = fs_old.promises;
 
 const router: Router = express.Router();
+
+const tmpConf = {
+  socketPath: process.env.MICRO_CI_SOCKET_PATH ?? (process.platform === "win32" ? "//./pipe/docker_engine" : "/var/run/docker.sock"),
+  userName: process.env.MICRO_CI_USERNAME ?? "",
+  password: process.env.MICRO_CI_PASSWORD ?? "",
+  dataPath: process.env.MICRO_CI_DATA ?? ".cache"
+};
+
+if (!fs_old.existsSync(tmpConf.dataPath)) {
+  fs.mkdir(tmpConf.dataPath);
+}
 
 export const get = (req: Request, res: Response) => {
     res.status(200).send({ j: "true" });
 };
 
-export const start = (req: Request, res: Response) => {
-    const createOptions: CreateOptions & FileOptions = {
-        file: "tarPath",
-        cwd: "gitPath"
-    };
+export const start = async (req: Request, res: Response) => {
+    if (req.query.repository === undefined && req.query.repository === "")
+    {
+      return res.status(500).send({ j: "invalid repository" });
+    }
+    const repository: string = req.query.repository as string;
+    console.log("Init - " + repository);
 
-    tar.create(createOptions, []);
+    const gitPath = path.join(tmpConf.dataPath, "gestao");
+    const tarPath = path.join(tmpConf.dataPath, "gestao.tar");
+
+    console.log("Cleaning files");
+    rimraf.sync(gitPath);
+    rimraf.sync(tarPath);
 
     const cloneOptions: CloneOptions = {};
     cloneOptions.fetchOpts = {
         callbacks: {
             certificateCheck: () => false,
-            credentials: () => Cred.userpassPlaintextNew(process.env.GIT_USER ?? "", process.env.GIT_PASS ?? ""),
+            credentials: () => Cred.userpassPlaintextNew(tmpConf.userName, tmpConf.password),
         }
     };
 
-    Clone.clone("", "gitPath", cloneOptions);
+    console.log("Cloning Repo");
+    await Clone.clone(repository, gitPath, cloneOptions);
 
-    const socketPath = process.env.SOCKET_PATH || (process.platform === "win32" ? "//./pipe/docker_engine" : " /var/run/docker.sock");
+    const createOptions: CreateOptions & FileOptions = {
+        file: tarPath,
+        cwd: gitPath
+    };
+
+    console.log("Compressing Repo");
+    const files = await fs.readdir(gitPath);
+
+    await tar.create({ file: tarPath, cwd: gitPath }, files);
 
     console.log("Building docker");
-    const docker: Docker = new Docker({ socketPath });
+    const docker = new Docker({ socketPath: tmpConf.socketPath });
 
-    res.status(200).send({ j: "true2" });
+    const stream = await docker.buildImage(tarPath, {t: ["microcl_gestao_5", "microcl_gestao_55"]});
+
+    await new Promise((resolve, reject) => {
+      docker.modem.followProgress(stream, onFinished, onProgress);
+
+      function onFinished(err: any, output: any) {
+        if (err) reject();
+        else resolve();
+      }
+      function onProgress(event: any) {
+        console.log(event);
+      }
+    });
+
+    return res.status(200).send({ j: "done" });
 };
 
 router.get("/", get);
